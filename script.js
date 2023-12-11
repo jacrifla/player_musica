d98f29752f6b97155d950f09d3662427b057d461
jacrifla <jaquelini_cris@hotmail.com>
2023-12-11 02:54:42 -0300

    salvar a playlist no localstorage do navegador

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const songName = document.getElementById("song-name");
-const bandName = document.getElementById("band-name");
-const song = document.getElementById("audio");
-const cover = document.getElementById("cover");
-const play = document.getElementById("play");
-const next = document.getElementById("next");
-const previous = document.getElementById("previous");
-const likeButton = document.getElementById("like");
-const currentProgress = document.getElementById("current-progress");
-const progressContainer = document.getElementById("progress-container");
-const shuffleButton = document.getElementById("shuffle");
-const repeatButton = document.getElementById("repeat");
-const songTime = document.getElementById("song-time");
-const totalTime = document.getElementById("total-time");
+// Declaração das variáveis que representam elementos HTML do reprodutor de música
+const songName = document.getElementById("song-name"); // Elemento que exibe o nome da música
+const bandName = document.getElementById("band-name"); // Elemento que exibe o nome da banda/artista
+const song = document.getElementById("audio"); // Elemento de áudio para reprodução
+const cover = document.getElementById("cover"); // Elemento de capa do álbum
+const play = document.getElementById("play"); // Botão de reproduzir/pausar música
+const next = document.getElementById("next"); // Botão de próxima música
+const previous = document.getElementById("previous"); // Botão de música anterior
+const likeButton = document.getElementById("like"); // Botão de "curtir" música
+const currentProgress = document.getElementById("current-progress"); // Barra de progresso atual
+const progressContainer = document.getElementById("progress-container"); // Container da barra de progresso
+const shuffleButton = document.getElementById("shuffle"); // Botão de embaralhar músicas
+const repeatButton = document.getElementById("repeat"); // Botão de repetir música
+const songTime = document.getElementById("song-time"); // Tempo atual da música
+const totalTime = document.getElementById("total-time"); // Tempo total da música
 
-
+// Lista original de músicas com informações como nome, artista, arquivo de áudio etc
 const originalPlaylist = [
   {
     songName: "Do it",
@@ -58,12 +59,27 @@ const originalPlaylist = [
     liked: false,
   },
 ];
+
+// Lista ordenada inicialmente com base na lista original
 let sortedPlaylist = [...originalPlaylist];
+
+// Recuperar a lista salva no armazenamento local, se existir
+const storedPlaylist = JSON.parse(localStorage.getItem('playlist'));
+
+// Verificar se há uma lista no armazenamento local e atribuir à lista ordenada, ou usar a lista original se não houver uma lista salva
+if (storedPlaylist && Array.isArray(storedPlaylist) && storedPlaylist.length > 0) {
+    sortedPlaylist = [...storedPlaylist]; // Atualizando a lista modificada
+} else {
+    sortedPlaylist = [...originalPlaylist]; // Se não houver lista salva, use a lista original
+}
+
+// Declaração de variáveis de controle e índice
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
 let index = 0;
 
+// Funções para controlar a reprodução de música, botões, etc.
 function playMusic() {
   play.querySelector(".bi").classList.remove("bi-play-circle-fill");
   play.querySelector(".bi").classList.add("bi-pause-circle-fill");
@@ -71,6 +87,7 @@ function playMusic() {
   isPlaying = true;
 }
 
+// Função para pausar música
 function pauseMusic() {
   play.querySelector(".bi").classList.add("bi-play-circle-fill");
   play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
@@ -78,6 +95,7 @@ function pauseMusic() {
   isPlaying = false;
 }
 
+// Função para decidir entre reproduzir ou pausar música
 function playPauseDecider() {
   if (isPlaying === true) {
     pauseMusic();
@@ -86,6 +104,7 @@ function playPauseDecider() {
   }
 }
 
+// Função para renderizar o botão de "curtir" a música
 function likeButtonRender() {
     if (sortedPlaylist[index].liked === true) {
         likeButton.querySelector('.bi').classList.remove('bi-heart')
@@ -99,6 +118,7 @@ function likeButtonRender() {
     }
 }
 
+// Função para tratar o clique no botão de "curtir"
 function likeButtonClicked() {
     if (sortedPlaylist[index].liked === false) {
         sortedPlaylist[index].liked = true;        
@@ -106,8 +126,10 @@ function likeButtonClicked() {
         sortedPlaylist[index].liked = false;
     }
     likeButtonRender();
+    localStorage.setItem('playlist', JSON.stringify(sortedPlaylist))
 }
 
+// Função para carregar informações da música
 function loadSong() {
   cover.src = `images/${sortedPlaylist[index].file}.jpg`;
   song.src = `songs/${sortedPlaylist[index].songFile}.mp3`;
@@ -116,6 +138,7 @@ function loadSong() {
   likeButtonRender();
 }
 
+// Funções para controle de música anterior e próxima
 function previousSong() {
   if (index === 0) {
     index = sortedPlaylist.length - 1;
@@ -136,12 +159,14 @@ function nextSong() {
   playMusic();
 }
 
+// Função para atualizar o progresso da música
 function updateProgress() {
     const barWidth = (song.currentTime / song.duration) * 100;
     currentProgress.style.setProperty('--progress', `${barWidth}%`);
     songTime.innerText = toHHMMSS(song.currentTime);
 }
 
+// Função para pular para uma posição específica na música
 function jumpTo(event) {
     const width = progressContainer.clientWidth;
     const clickPosition = event.offsetX;
@@ -149,6 +174,7 @@ function jumpTo(event) {
     song.currentTime = jumpToTime;
 }
 
+// Função para embaralhar um array
 function shuffleArray(preShuffleArray) {
     const size = preShuffleArray.length;
     let currentIndex = size - 1;
@@ -161,6 +187,7 @@ function shuffleArray(preShuffleArray) {
     }
 }
 
+// Função para embaralhar a lista de reprodução
 function shuffleButtonClicked() {
     if (isShuffled === false) {
         isShuffled = true;
@@ -174,6 +201,7 @@ function shuffleButtonClicked() {
     }
 }
 
+// Função para alternar o modo de repetição
 function repeatButtonClicked() {
     if (repeatOn === false) {
         repeatOn = true;
@@ -185,6 +213,7 @@ function repeatButtonClicked() {
     }
 }
 
+// Função para decidir entre reproduzir a próxima música ou repetir a atual
 function nextOrRepeat() {
     if (repeatOn === false) {
         nextSong();
@@ -193,6 +222,7 @@ function nextOrRepeat() {
     }
 }
 
+// Função para formatar o tempo em horas, minutos e segundos
 function toHHMMSS(originalNumber) {
     let hours = Math.floor(originalNumber / 3600);
     let minutes = Math.floor((originalNumber - hours * 3600) / 60);
@@ -201,12 +231,15 @@ function toHHMMSS(originalNumber) {
     return `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${segundos.toString().padStart(2, 0)}`
 }
 
+// Função para atualizar o tempo total da música
 function updateTotalTime() {
     totalTime.innerText = toHHMMSS(song.duration);    
 }
 
+// Carregar informações da primeira música na inicialização
 loadSong();
 
+// Event listeners para os elementos HTML
 play.addEventListener("click", playPauseDecider);
 previous.addEventListener("click", previousSong);
 next.addEventListener("click", nextSong);
